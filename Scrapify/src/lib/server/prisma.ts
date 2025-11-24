5166c290d4c9d5fff051794b72424dfa56fc7109
tomix421q <zilka.tomas421@gmail.com>
2025-11-24 13:31:51 +0100

    prod

--- Scrapify/src/lib/server/prisma.ts
+++ Scrapify/src/lib/server/prisma.ts
@@ -11,7 +11,7 @@ const createPrismaClientWithAdapter = () => {
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
-		server: process.env.HOST!, // Dôležité: Uistite sa, že HOST je vždy definovaný
+		server: process.env.DB_HOST!, // Dôležité: Uistite sa, že HOST je vždy definovaný
 		port: Number(process.env.DB_PORT) || 1433,
 		pool: {
 			max: 10,
@@ -23,7 +23,7 @@ const createPrismaClientWithAdapter = () => {
 			trustServerCertificate: true // true pre lokálne (ak nemáš certifikát)
 		}
 	};
-	console.log(sqlConfig)
+	console.log(sqlConfig);
 
 	const adapter = new PrismaMssql(sqlConfig);
 	return new PrismaClient({ adapter });
